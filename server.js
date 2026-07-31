9b14a3c93d84753b2355c356417a7e66cbc62f33
gurtegsinghsodi2016-gp <gurtegsinghsodi2016@gmail.com>
2026-07-31 15:27:40 +0530

    Add server.js

--- server.js
+++ server.js
@@ -1,24 +1,70 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
 
 app.get("/", (req, res) => {
-  res.send("Java Compiler Backend is Running!");
+    res.send("Java Compiler Backend Running");
 });
 
-app.post("/run", async (req, res) => {
-  res.json({
-    success: false,
-    message: "Java compiler is not configured yet."
-  });
-});
+app.post("/run", (req, res) => {
 
+    const code = req.body.code;
+
+    if (!code) {
+        return res.status(400).json({
+            output: "No Java code received."
+        });
+    }
+
+    const folder = path.join(__dirname, "temp");
+
+    if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder);
+    }
+
+    const javaFile = path.join(folder, "Main.java");
+
+    fs.writeFileSync(javaFile, code);
+
+    exec(
+        `javac ${javaFile}`,
+              (compileError, compileStdout, compileStderr) => {
+
+            if (compileError) {
+                return res.json({
+                    output: compileStderr
+                });
+            }
+
+            exec(
+                `java -cp ${folder} Main`,
+                (runError, runStdout, runStderr) => {
+
+                    if (runError) {
+                        return res.json({
+                            output: runStderr
+                        });
+                    }
+
+                    return res.json({
+                        output: runStdout
+                    });
+
+                }
+            );
+
+        }
+    );
+})
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
